7c904f1fdfb0f48076d0e6ebd7c6ced5fb1216e4
jonhvtr <jonhvtr_dev@hotmail.com>
2026-05-16 23:02:59 -0300

    refactor(task): replace status and priority strings with enums

--- src/main/java/com/jonhvtr/todolist/domain/dto/TaskUpdate.java
+++ src/main/java/com/jonhvtr/todolist/domain/dto/TaskUpdate.java
@@ -1,16 +1,16 @@
 package com.jonhvtr.todolist.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotNull;
+import com.jonhvtr.todolist.domain.enums.Priority;
+import com.jonhvtr.todolist.domain.enums.Status;
 
 import java.time.LocalDateTime;
 
 public record TaskUpdate(
-        @NotNull Long id,
         String title,
         String content,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm") LocalDateTime dueDate,
-        String status,
-        String priority,
+        Status status,
+        Priority priority,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm") LocalDateTime reminderDateTime) {
 }
