fb6860f6b774c473be809b0925ee4d1eeb843d8b
jonhvtr <jonhvtr_dev@hotmail.com>
2026-05-16 23:20:43 -0300

    refactor: remove unused import from application class

--- src/main/java/com/jonhvtr/todolist/TodolistApplication.java
+++ src/main/java/com/jonhvtr/todolist/TodolistApplication.java
@@ -1,6 +1,5 @@
 package com.jonhvtr.todolist;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
